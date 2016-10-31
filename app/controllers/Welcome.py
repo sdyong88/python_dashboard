f4409d1779a0e8e262b6f1b0c9c8e2b44c10ebbd
sdyong88 <sdyong88@gmail.com>
2016-10-31 12:40:19 -0700

    First Commit

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -6,7 +6,9 @@
 
     Create a controller using this template
 """
+from time import strftime
 from system.core.controller import *
+
 
 class Welcome(Controller):
     def __init__(self, action):
@@ -21,20 +23,13 @@ class Welcome(Controller):
         """
         
         This is an example of a controller method that will load a view for the client 
-
         """
-   
     def index(self):
-        """
-        A loaded model is accessible through the models attribute 
-        self.models['WelcomeModel'].get_users()
-        
-        self.models['WelcomeModel'].add_message()
-        # messages = self.models['WelcomeModel'].grab_messages()
-        # user = self.models['WelcomeModel'].get_user()
-        # to pass information on to a view it's the same as it was with Flask
-        
-        # return self.load_view('index.html', messages=messages, user=user)
-        """
         return self.load_view('index.html')
+
+
+    
+        
+
+    
 
